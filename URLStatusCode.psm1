49353d1ff656fd8a636982040c9dc4ae34b0f8fc
MiRUTH <49371920+miruthde@users.noreply.github.com>
2021-01-22 16:02:40 +0100

    Add files via upload

---
+++ URLStatusCode.psm1
@@ -0,0 +1,11 @@
+﻿    function Get-UrlStatusCode([string] $Url)
+    {
+    try
+    {
+        (Invoke-WebRequest -Uri $Url -UseBasicParsing -DisableKeepAlive).StatusCode
+    }
+    catch [Net.WebException]
+    {
+        [int]$_.Exception.Response.StatusCode
+    }
+    }
